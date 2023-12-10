aefa83147b6927a50bcf5a30179455398df31b45
rom-30 <r.stalbin@gmail.com>
2023-12-10 14:09:46 +0000

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,18 +7,23 @@ jest.mock('axios')
 axios.get.mockResolvedValue({ data: { message: [] } })
 
 describe('App', () => {
+  /*global renderWithReduxProvider */
 
   test('it renders a clickable heading to refresh content', () => {
     renderWithReduxProvider(<App />)
     const heading = screen.getByRole('heading', { name: 'refresh' })
+
     userEvent.click(heading)
+
     expect(axios.get).toHaveBeenCalled()
   })
 
   test('it renders error if error existts', () => {
     const initState = { loading: true, doggos: [], error: new Error('Bad Things') }
     renderWithReduxProvider(<App />, { initState })
+
     const error = screen.getByRole('alert')
+
     expect(error).toBeInTheDocument()
     expect(error.textContent).toBe('Bad Things')
   })
--- src/components/LikeButton/LikeButton.test.js
+++ src/components/LikeButton/LikeButton.test.js
@@ -7,20 +7,26 @@ describe('LikeButton', () => {
 
   test('it calls a handleClick prop when clicked', () => {
     render(<LikeButton handleClick={stubHandleClick} />);
+
     let btn = screen.getByRole('button', { name: 'like' })
     fireEvent.click(btn)
+
     expect(stubHandleClick).toHaveBeenCalledTimes(1);
   })
 
   test('it renders red content if liked prop is truthy', () => {
     render(<LikeButton liked={true} handleClick={stubHandleClick} />);
+
     let btn = screen.getByRole('button', { name: 'like' })
+
     expect(btn.style.color).toBe('red');
   })
 
   test('it renders white content if liked prop is falsy', () => {
     render(<LikeButton liked={false} handleClick={stubHandleClick} />);
+
     let btn = screen.getByRole('button', { name: 'like' })
+
     expect(btn.style.color).toBe('white');
   })
 });
